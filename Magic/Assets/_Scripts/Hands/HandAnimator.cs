dfcc2a1f70bbee126e59b2b9095510412ecfab40
Philip <philip@nobrakesgames.com>
2023-01-15 16:02:37 +0000

    stable

--- Magic/Assets/_Scripts/Hands/HandAnimator.cs
+++ Magic/Assets/_Scripts/Hands/HandAnimator.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace VR.System
@@ -32,8 +30,6 @@ namespace VR.System
         int alphaMultiplierId = Shader.PropertyToID("_AlphaMultiplier");
 
         MaterialPropertyBlock propertyBlock;
-
-        const float grabAmountForFist = 0.8f;
 
         AnimatedFinger[] allFingers;
 
